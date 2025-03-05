59166a79bd56d3e690ff77c844931d311e616a77
jendyx <jan.skolil06@seznam.cz>
2025-03-05 19:16:58 +0100

    initial commit

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const apikey = "59af4c678e0561258b55208d0a47924e";
 const imageBaseUrl = "https://image.tmdb.org/t/p/w300";
 
 const moviesGrid = document.getElementById("movies-grid");
+const tvShowsGrid = document.getElementById("tvshows-grid");
 const searchInput = document.getElementById("search-input");
 const searchForm = document.getElementById("search-form");
 
@@ -36,11 +37,42 @@ function displayMovies(movies) {
         .join("");
 }
 
+
+async function fetchTvShowsAiringToday() {
+    const response = await fetch(`${apiBaseUrl}/tv/airing_today?api_key=${apikey}`);
+    const jsonResponse = await response.json();
+    const tvShows = jsonResponse.results;
+    
+    displayTvShows(tvShows);
+}
+
+async function searchTvShows(query) {
+    const response = await fetch(`${apiBaseUrl}/search/tv?api_key=${apikey}&query=${query}`);
+    const jsonResponse = await response.json();
+    const tvShows = jsonResponse.results;
+    
+    displayTvShows(tvShows);
+}
+
+function displayTvShows(tvShows) {
+    tvShowsGrid.innerHTML = tvShows
+        .map(show => 
+            `<div class="tvshow-card">
+            <img src="${imageBaseUrl}${show.poster_path}"/>
+            <p>*${show.vote_average}</p>
+            <h1>${show.name}</h1>
+            </div>`
+        )
+        .join("");
+}
+
 function handleSearchFormSubmit(event) {
     event.preventDefault();
     const searchQuery = searchInput.value;
     const movies = searchMovies(searchQuery);
+    const tvShows =  searchTvShows(searchQuery);
 }
 
 searchForm.addEventListener("submit", handleSearchFormSubmit);
 fetchMoviesNowPlaying();
+fetchTvShowsAiringToday();
